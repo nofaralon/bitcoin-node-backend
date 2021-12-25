f61db5b8ce5eb8040eb5669c27c21c486051b3cf
Nofar Alon <nofaralon28@gmail.com>
2021-12-25 20:38:39 +0200

    add public folder

---
+++ public/precache-manifest.05a488a7ef6e217698ae2759ddab7abc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a396dd654f4a3efcd8b9",
+    "url": "/css/app.565d4e4f.css"
+  },
+  {
+    "revision": "f323e0363c44726a45d0",
+    "url": "/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "cb17fa4a508de3daf0d9ea64a0ab724e",
+    "url": "/fonts/Manrope-Bold.cb17fa4a.ttf"
+  },
+  {
+    "revision": "29b7f4fc75797a82d890da507b02ae92",
+    "url": "/fonts/Manrope-VariableFont_wght.29b7f4fc.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "9765478f70ddf9a713f466180740805a",
+    "url": "/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "34b33e5471b4d104747eba6780bc4af6",
+    "url": "/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "02af85b562acbd96376ae3ff6d3e9a5a",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "6ad133db2e527132ff545b05341867bd",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "1cbb371c5019b693f1f06994af88afdd",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "0d8ff69d0513eaf2fef2459dcdd816b3",
+    "url": "/img/logo.0d8ff69d.png"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/img/site.webmanifest"
+  },
+  {
+    "revision": "ce51468db67c658ae7c4265eca265892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a396dd654f4a3efcd8b9",
+    "url": "/js/app.72999d39.js"
+  },
+  {
+    "revision": "f323e0363c44726a45d0",
+    "url": "/js/chunk-vendors.695c31de.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
